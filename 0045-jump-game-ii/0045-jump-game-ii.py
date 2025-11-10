55186dbda7d7809a39f2302aad9096eeafe2b0c6
Dev Bhardwaj <bhardwajdev046@gmail.com>
2025-11-11 01:10:49 +0530

    Time: 7 ms (79.28%), Space: 18.6 MB (75.02%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,33 @@
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+        # cache={}
+        # def helper(i,nums):
+        #     if i>=len(nums)-1:
+        #         return 0
+        #     if i in cache:                # DP Solution
+        #         return cache[i]
+        #     if nums[i]==0:
+        #         return float('inf')
+        #     min_steps=float('inf')
+        #     for s in range(1,nums[i]+1):
+        #         ans=1+helper(i+s,nums)
+        #         min_steps=min(min_steps,ans)
+
+        #     cache[i]=min_steps
+        #     return min_steps
+        # return helper(0,nums)
+
+                     
+                                   #GREEDY Solution           
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        cache={}
-        def helper(i,nums):
-            if i>=len(nums)-1:
-                return 0
-            if i in cache:
-                return cache[i]
-            if nums[i]==0:
-                return float('inf')
-            min_steps=float('inf')
-            for s in range(1,nums[i]+1):
-                ans=1+helper(i+s,nums)
-                min_steps=min(min_steps,ans)
-
-            cache[i]=min_steps
-            return min_steps
-        return helper(0,nums)
+        left,right=0,0
+        jump=0
+        while right<len(nums)-1:
+            farthest=0
+            for i in range(left,right+1):
+                farthest=max(farthest,i+nums[i])
+            left=right+1
+            right=farthest
+            jump+=1
+        return jump
