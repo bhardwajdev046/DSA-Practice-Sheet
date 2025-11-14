d5481f1432d02421d941bd34716ef7fb664dc1f8
Dev Bhardwaj <bhardwajdev046@gmail.com>
2025-11-14 15:53:53 +0530

    Time: 3 ms (34.78%), Space: 19.8 MB (68.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,18 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        li=[]
-        def inorder(root):
-            if root is None:
-                return
-            inorder(root.left)
-            li.append(root.val)
-            inorder(root.right)
+    def solve(self,temp,l,r):
+        if temp is None:
+            return True
+        if not l<temp.val<r:
+            return False
+        left=self.solve(temp.left,l,temp.val)
+        if left==False:
+            return False
+        right=self.solve(temp.right,temp.val,r)
+        return left and right
 
-        inorder(root)
-        for i in range(1,len(li)):
-            if li[i] <= li[i-1]:
-                return False
-        return True
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        l=float('-inf')
+        r=float('inf')
+        return self.solve(root,l,r)
+        # li=[]
+        # def inorder(root):
+        #     if root is None:
+        #         return
+        #     inorder(root.left)
+        #     li.append(root.val)
+        #     inorder(root.right)
+
+        # inorder(root)
+        # for i in range(1,len(li)):
+        #     if li[i] <= li[i-1]:
+        #         return False
+        # return True
         
