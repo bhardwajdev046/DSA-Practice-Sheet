555ad71f2ee268f1f5def80f2a01a66f2096b960
Dev Bhardwaj <bhardwajdev046@gmail.com>
2025-11-25 00:02:27 +0530

    Time: 2 ms (59.51%), Space: 18.6 MB (13.69%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -1,23 +1,28 @@
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         def reverse(char,i,j):
+#             while i<j:
+#                 char[i],char[j] = char[j],char[i]
+#                 i+=1
+#                 j-=1
+#             return "".join(char)
+#             # res=""
+#             # for i in string[::-1]:
+#             #     res+=i
+#             # return res
+
+#         i=0
+#         char=list(s)
+#         n=len(char)
+#         for j in range(n+1):
+#             if j==n or char[j]==" ":
+#                 reverse(char,i,j-1)
+#                 i=j+1
+#         return "".join(char)
+        
 class Solution:
     def reverseWords(self, s: str) -> str:
-        def reverse(char,i,j):
-            while i<j:
-                char[i],char[j] = char[j],char[i]
-                i+=1
-                j-=1
-            return "".join(char)
-            # res=""
-            # for i in string[::-1]:
-            #     res+=i
-            # return res
-
-        i=0
-        char=list(s)
-        n=len(char)
-        for j in range(n+1):
-            if j==n or char[j]==" ":
-                reverse(char,i,j-1)
-                i=j+1
-        return "".join(char)
-        
+        word=s.split()
+        reverse=[w[::-1] for w in word]
+        return " ".join(reverse)
         
