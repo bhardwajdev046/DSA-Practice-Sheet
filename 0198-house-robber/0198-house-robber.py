2cabf67b4535fe99a7b72f4b7ab2e033b9e03dbb
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-01-26 01:41:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (9.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -14,20 +14,55 @@ class Solution:
         # idx=len(nums)-1
         # return fun(idx,nums)
 
-        def fun(idx,nums,dp):
-            if idx==0:
-                return nums[0]
-            if idx<0:
-                return 0
-            if dp[idx]!=-1:
-                return dp[idx]
+        # def fun(idx,nums,dp):
+        #     if idx==0:
+        #         return nums[0]
+        #     if idx<0:
+        #         return 0
+        #     if dp[idx]!=-1:
+        #         return dp[idx]
 
-            rob = nums[idx] + fun(idx-2,nums,dp)
-            skip = 0 + fun(idx-1,nums,dp)
+        #     rob = nums[idx] + fun(idx-2,nums,dp)
+        #     skip = 0 + fun(idx-1,nums,dp)
 
-            dp[idx]=max(rob,skip)
-            return dp[idx]
+        #     dp[idx]=max(rob,skip)
+        #     return dp[idx]
+        
+        # idx=len(nums)-1
+        # dp=[-1]*(idx+1)
+        # return fun(idx,nums,dp)
+
+        # def fun(idx,nums):
+        #     dp=[-1]*(idx+1)
+        #     dp[0]=nums[0]
+
+        #     for i in range(1,idx+1):
+        #         if i>1:
+        #             rob = nums[i] + dp[i-2]
+        #         else:
+        #             rob=nums[i]
+        #         skip= 0 + dp[i-1]
+
+        #         dp[i]=max(rob, skip)
+        #     return dp[idx]
+        
+        # idx=len(nums)-1
+        # return fun(idx,nums)
+
+        def fun(idx,nums):
+            prev=nums[0]
+            prev2=0
+
+            for i in range(1,idx+1):
+                if i>1:
+                    rob = nums[i] + prev2
+                else:
+                    rob=nums[i]
+                skip= 0 + prev
+                current=max(rob, skip)
+                prev2=prev
+                prev=current     
+            return prev
         
         idx=len(nums)-1
-        dp=[-1]*(idx+1)
-        return fun(idx,nums,dp)
+        return fun(idx,nums)
