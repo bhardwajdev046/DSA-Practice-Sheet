a6249c908eb4e1ee9c6c4164c1c6c3f97664aa83
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-02-02 02:14:58 +0530

    Time: 0 ms (100%), Space: 19.5 MB (15.99%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,19 +1,39 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        def fun(i,j,obstacleGrid,dp):
-            if i<0 or j<0:
+        # def fun(i,j,obstacleGrid,dp):
+        #     if i<0 or j<0:
+        #         return 0
+        #     if obstacleGrid[i][j]==1:
+        #         return 0
+        #     if i==0 and j==0:
+        #         return 1
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     left=fun(i,j-1,obstacleGrid,dp)
+        #     up=fun(i-1,j,obstacleGrid,dp)
+        #     dp[i][j]=left+up
+        #     return dp[i][j]
+        # i=len(obstacleGrid)
+        # j=len(obstacleGrid[0])
+        # dp=[[-1]*j for _ in range(i)]
+        # return fun(i-1,j-1,obstacleGrid,dp)
+
+        def fun(row,col,obstacleGrid):
+            if obstacleGrid[0][0] == 1:
                 return 0
-            if obstacleGrid[i][j]==1:
-                return 0
-            if i==0 and j==0:
-                return 1
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            left=fun(i,j-1,obstacleGrid,dp)
-            up=fun(i-1,j,obstacleGrid,dp)
-            dp[i][j]=left+up
-            return dp[i][j]
+            dp=[[-1]*col for _ in range(row)]
+            dp[0][0]=1
+            for i in range(row):
+                for j in range(col):
+                    if i==0 and j==0:
+                        continue
+                    if obstacleGrid[i][j]==1:
+                        dp[i][j]=0
+                        continue
+                    left=dp[i][j-1] if j>0 else 0
+                    up=dp[i-1][j] if i>0 else 0
+                    dp[i][j]=left+up
+            return dp[row-1][col-1]
         i=len(obstacleGrid)
         j=len(obstacleGrid[0])
-        dp=[[-1]*j for _ in range(i)]
-        return fun(i-1,j-1,obstacleGrid,dp)
+        return fun(i,j,obstacleGrid)
