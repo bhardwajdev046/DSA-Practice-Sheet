9a5b059f14f37c3915e336ba0b895c2d223f579b
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-02-02 01:49:37 +0530

    Time: 3 ms (80.19%), Space: 20.2 MB (33.83%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -15,18 +15,35 @@ class Solution:
         # dp=[[-1]*b for _ in range(a)]
         # return fun(0,0,triangle,dp)
 
-        def fun(m,n,ar):
-            a=len(ar)
-            b=len(ar[a-1])
-            dp=[[-1]*b for _ in range(a)]
-            for k in range(b):
-                dp[a-1][k]=ar[a-1][k]
+        # def fun(m,n,ar):
+        #     row=len(ar)
+        #     col=len(ar[row-1])
+        #     dp=[[-1]*col for _ in range(row)]
+        #     for k in range(col):
+        #         dp[row-1][k]=ar[row-1][k]
 
-            for i in range(a-2,-1,-1):
-                for j in range(i,-1,-1):
-                    down=dp[i+1][j] if i<a-1 else float('inf')
-                    right=dp[i+1][j+1] if (i<a-1 and j<b) else float('inf')
-                    dp[i][j]=ar[i][j]+min(down,right)
-            return dp[0][0] 
+        #     for i in range(row-2,-1,-1):
+        #         for j in range(i,-1,-1):
+        #             down=dp[i+1][j] if i<row-1 else float('inf')
+        #             right=dp[i+1][j+1] if (i<row-1 and j<col) else float('inf')
+        #             dp[i][j]=ar[i][j]+min(down,right)
+        #     return dp[0][0] 
+        # return fun(0,0,triangle)
+
+        def fun(m,n,ar):
+            row=len(ar)
+            col=len(ar[row-1])
+            prev=[0]*col
+            for k in range(col):
+                prev[k]=ar[row-1][k]
+
+            for i in range(row-2,-1,-1):
+                curr=[0]*(i+1)
+                for j in range(0,i+1):
+                    down=prev[j] 
+                    right=prev[j+1]
+                    curr[j]=ar[i][j]+min(down,right)
+                prev=curr
+            return prev[m] 
         return fun(0,0,triangle)
         
