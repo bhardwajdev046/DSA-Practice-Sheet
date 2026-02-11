8ab55a3a68775b921ddb944129b912579e244cd9
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-02-11 23:31:49 +0530

    Time: 510 ms (41.19%), Space: 67 MB (24.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,36 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        dp=[[-1]*(amount+1) for _ in range(n)]
-        def fun(i,amount):
-            if i==0:
-                if amount%coins[0]==0:
-                    return 1
-                return 0
-            if dp[i][amount]!=-1:
-                return dp[i][amount]
-            pick=0
-            if amount>=coins[i]:
-                pick=fun(i,amount-coins[i])
-            notpick=fun(i-1,amount)
-            dp[i][amount]=pick+notpick
-            return dp[i][amount]
+        # dp=[[-1]*(amount+1) for _ in range(n)]
+        # def fun(i,amount):
+        #     if i==0:
+        #         if amount%coins[0]==0:
+        #             return 1
+        #         return 0
+        #     if dp[i][amount]!=-1:
+        #         return dp[i][amount]
+        #     pick=0                    
+        #     if amount>=coins[i]:
+        #         pick=fun(i,amount-coins[i])
+        #     notpick=fun(i-1,amount)
+        #     dp[i][amount]=pick+notpick
+        #     return dp[i][amount]
+
+        def fun(idx,amount):
+            dp=[[-1]*(amount+1) for _ in range(n)]
+            for i in range(amount+1):
+                if i%coins[0]==0:
+                    dp[0][i]=1
+                else:
+                    dp[0][i]=0
+            for i in range(1,idx+1):
+                for amt in range(amount+1):
+                    pick=0                    
+                    if amt>=coins[i]:
+                        pick=dp[i][amt-coins[i]]
+                    notpick=dp[i-1][amt]
+                    dp[i][amt]=pick+notpick
+            return dp[idx][amount]
+
     
         return fun(n-1,amount)
