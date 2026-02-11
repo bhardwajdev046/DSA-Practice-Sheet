9cec864ef0823b8b73059eada865d4389f0748a0
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-02-11 23:55:22 +0530

    Time: 385 ms (51.97%), Space: 19.7 MB (60.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -16,21 +16,38 @@ class Solution:
         #     dp[i][amount]=pick+notpick
         #     return dp[i][amount]
 
+        # def fun(idx,amount):
+        #     dp=[[-1]*(amount+1) for _ in range(n)]
+        #     for i in range(amount+1):
+        #         if i%coins[0]==0:
+        #             dp[0][i]=1
+        #         else:
+        #             dp[0][i]=0
+        #     for i in range(1,idx+1):
+        #         for amt in range(amount+1):
+        #             pick=0                    
+        #             if amt>=coins[i]:
+        #                 pick=dp[i][amt-coins[i]]
+        #             notpick=dp[i-1][amt]
+        #             dp[i][amt]=pick+notpick
+        #     return dp[idx][amount]
+
         def fun(idx,amount):
-            dp=[[-1]*(amount+1) for _ in range(n)]
+            prev=[0]*(amount+1)
             for i in range(amount+1):
                 if i%coins[0]==0:
-                    dp[0][i]=1
-                else:
-                    dp[0][i]=0
+                    prev[i]=1
+
             for i in range(1,idx+1):
+                curr=[0]*(amount+1)
                 for amt in range(amount+1):
                     pick=0                    
                     if amt>=coins[i]:
-                        pick=dp[i][amt-coins[i]]
-                    notpick=dp[i-1][amt]
-                    dp[i][amt]=pick+notpick
-            return dp[idx][amount]
+                        pick=curr[amt-coins[i]]
+                    notpick=prev[amt]
+                    curr[amt]=pick+notpick
+                prev=curr
+            return prev[amount]
 
     
         return fun(n-1,amount)
