e3d70c38b042db665acaf9e6b887071e2daf1d76
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-02-14 22:51:07 +0530

    Time: 342 ms (75.82%), Space: 19.4 MB (68.33%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,15 +1,27 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
         s1=s[::-1]
+        # def fun(m,n):
+        #     dp=[[0]*(n+1) for _ in range(m+1)]
+        #     for i in range(1,m+1):
+        #         for j in range(1,n+1):
+        #             if s[i-1]==s1[j-1]:
+        #                 dp[i][j]=1+dp[i-1][j-1]
+        #             else:
+        #                 dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        #     return dp[m][n]
+
         def fun(m,n):
-            dp=[[0]*(n+1) for _ in range(m+1)]
+            prev=[0]*(n+1)
             for i in range(1,m+1):
+                curr=[0]*(n+1)
                 for j in range(1,n+1):
                     if s[i-1]==s1[j-1]:
-                        dp[i][j]=1+dp[i-1][j-1]
+                        curr[j]=1+prev[j-1]
                     else:
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-            return dp[m][n]
+                        curr[j]=max(prev[j],curr[j-1])
+                prev=curr
+            return prev[n]
 
         temp=fun(len(s),len(s1))
         return len(s)-temp
