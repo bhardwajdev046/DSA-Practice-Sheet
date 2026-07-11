bfda7ed3ab7955bf254daa83403bf14710975b0a
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-07-12 01:41:27 +0530

    Time: 19 ms (61.85%), Space: 19.6 MB (71.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,26 @@
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         count = [0] * 26
+
+#         for ch in magazine:
+#             count[ord(ch) - ord('a')] += 1
+
+#         for ch in ransomNote:
+#             if count[ord(ch) - ord('a')] != 0:
+#                 count[ord(ch) - ord('a')] -= 1
+#             else:
+#                 return False
+
+#         return True
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        count = [0] * 26
-
+        count = {}
         for ch in magazine:
-            count[ord(ch) - ord('a')] += 1
-
+            count[ch] = count.get(ch,0) + 1            
         for ch in ransomNote:
-            if count[ord(ch) - ord('a')] != 0:
-                count[ord(ch) - ord('a')] -= 1
-            else:
-                return False
+            if count.get(ch,0) == 0 : 
+                return False            
+            count[ch] -= 1
 
         return True
