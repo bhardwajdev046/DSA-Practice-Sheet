5ab1514f5eb79e55f0fdb453a94e54bc91ce62e3
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-08-03 17:48:29 +0530

    Time: 6 ms (56.92%), Space: 23.1 MB (28.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,29 @@
+import heapq
+class pair:
+    def __init__(self,first,second):
+        self.first=first
+        self.second=second
+    def __lt__(self,other):
+        if self.first!=other.first:
+            return self.first < other.first
+        return self.second < other.second
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hash={}
-        res=[]
-        
         for i in nums:
-            if i in hash:
-                hash[i]+=1
-            else:
-                hash[i]=1
-        temp=sorted(hash.items(),key=lambda x:x[1],reverse=True)
-        for i in range(k):
-            res.append(temp[i][0])
-        return res
+            hash[i]=hash.get(i,0)+1
+        heap=[]
+        for ele,freq in hash.items():
+            curr = pair(freq,ele)
+            if len(heap)<k:
+                heapq.heappush(heap, curr)
+                continue
+            if curr.first < heap[0].first:
+                continue
+            heapq.heappop(heap)
+            heapq.heappush(heap, curr)
+
+        ans=[]
+        while heap:
+            ans.append(heapq.heappop(heap).second)
+        return ans
