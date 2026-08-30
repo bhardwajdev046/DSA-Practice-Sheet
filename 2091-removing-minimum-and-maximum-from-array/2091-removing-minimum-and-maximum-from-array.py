d9ad61bf4c7081c337a6112a728e9a21e2c983e7
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-08-31 03:22:07 +0530

    Time: 19 ms (65.68%), Space: 33.7 MB (35.5%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,20 +1,35 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        maxi=max(nums)
-        mini=min(nums)
-        n=len(nums)
-        hash={}
-        for i in range(len(nums)):
-            if nums[i] == maxi or nums[i] == mini:
-                hash[nums[i]]=i
-        print(hash)
-        c1,c2,c3 = 0,0,0
-        c1 = max(hash[maxi],hash[mini])+1
-        c2 = max((n-hash[maxi]), (n-hash[mini])) 
-        t1, t2 = 0, 0
-        t1 = min(hash[maxi], hash[mini])+1
-        t2 = min((n-hash[maxi]), (n-hash[mini]))
-        c3 = t1+t2
-        print((t1,t2))
-        print([c1,c2])
-        return min(c1,c2,c3)
+        # maxi=max(nums)
+        # mini=min(nums)
+        # n=len(nums)
+        # hash={}
+        # for i in range(len(nums)):
+        #     if nums[i] == maxi or nums[i] == mini:
+        #         hash[nums[i]]=i
+        # print(hash)
+        # c1,c2,c3 = 0,0,0
+        # c1 = max(hash[maxi],hash[mini])+1
+        # c2 = max((n-hash[maxi]), (n-hash[mini])) 
+        # t1, t2 = 0, 0
+        # t1 = min(hash[maxi], hash[mini])+1
+        # t2 = min((n-hash[maxi]), (n-hash[mini]))
+        # c3 = t1+t2
+        # print((t1,t2))
+        # print([c1,c2])
+        # return min(c1,c2,c3)
+
+        n = len(nums)
+        minIndex = nums.index(min(nums))
+        maxIndex = nums.index(max(nums))
+        left = min(minIndex, maxIndex)
+        right = max(minIndex, maxIndex)
+        # Remove both from the front
+        front = right + 1
+
+        # Remove both from the back
+        back = n - left
+
+        # Remove one from each side
+        frontBack = (left + 1) + (n - right)
+        return min(front, back, frontBack)
