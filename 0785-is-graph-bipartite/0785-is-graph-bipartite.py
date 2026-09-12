5bc09f01701aa069bcd4f4bf0ab7fe5f87bd3e9e
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-09-12 12:25:45 +0530

    Time: 0 ms (100%), Space: 19.7 MB (78.97%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,24 +1,43 @@
 class Solution:
-    def dfs(self, node, visited, graph, color):
-        visited[node]=color
-        for x in graph[node]:
-            if visited[x]!=-1:
-                if visited[x]==color:
-                    return False
-            else:
-                ans = self.dfs(x,visited,graph,1-color)
-                if ans==False:
-                    return False
-        return True
+    # def dfs(self, node, visited, graph, color):
+    #     visited[node]=color
+    #     for x in graph[node]:
+    #         if visited[x]!=-1:
+    #             if visited[x]==color:
+    #                 return False
+    #         else:
+    #             ans = self.dfs(x,visited,graph,1-color)
+    #             if ans==False:
+    #                 return False
+    #     return True
 
 
+    # def isBipartite(self, graph: List[List[int]]) -> bool:
+    #     n = len(graph)
+    #     visited = [-1]*n
+    #     for node in range(n):
+    #         if visited[node]!=-1:
+    #             continue
+    #         ans = self.dfs(node, visited, graph, 0)
+    #         if ans==False:
+    #             return False
+    #     return True
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        visited = [-1]*n
+        visited=[-1]*n
         for node in range(n):
-            if visited[node]!=-1:
+            if visited[node] != -1:
                 continue
-            ans = self.dfs(node, visited, graph, 0)
-            if ans==False:
-                return False
+            q = deque()
+            q.append(node)
+            visited[node]=0
+            while q:
+                cur_node = q.popleft()
+                for x in graph[cur_node]:
+                    if visited[x]!=-1:
+                        if visited[x] == visited[cur_node]:
+                            return False
+                    else:
+                        visited[x] = 1-visited[cur_node]
+                        q.append(x)
         return True
