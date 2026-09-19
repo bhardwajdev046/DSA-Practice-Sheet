be398024f99aaa2c1262c684b2e96a53532660b6
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-09-19 22:29:10 +0530

    Time: 247 ms (47.99%), Space: 21.5 MB (73.5%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,54 @@
-class Solution:
-    def bfs(self,i,j,grid,visited):
-        q = deque()
-        q.append((i,j))
-        visited[i][j]=1
-        while q:
-            r,c = q.popleft()
-            for xz,yz in((0,-1),(0,1),(-1,0),(1,0)):
-                new_x = r+xz
-                new_y = c+yz
-                if new_x<0 or new_x>=len(grid) or new_y<0 or new_y>=len(grid[0]):
-                    continue
-                if visited[new_x][new_y]==1:
-                    continue
-                if grid[new_x][new_y]=='0':
-                    continue
-                q.append((new_x,new_y))
-                visited[new_x][new_y]=1
+# class Solution:
+#     def bfs(self,i,j,grid,visited):
+#         q = deque()
+#         q.append((i,j))
+#         visited[i][j]=1
+#         while q:
+#             r,c = q.popleft()
+#             for xz,yz in((0,-1),(0,1),(-1,0),(1,0)):
+#                 new_x = r+xz
+#                 new_y = c+yz
+#                 if new_x<0 or new_x>=len(grid) or new_y<0 or new_y>=len(grid[0]):
+#                     continue
+#                 if visited[new_x][new_y]==1:
+#                     continue
+#                 if grid[new_x][new_y]=='0':
+#                     continue
+#                 q.append((new_x,new_y))
+#                 visited[new_x][new_y]=1
 
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         rows = len(grid)
+#         cols = len(grid[0])
+#         visited = [[0]*cols for _ in range(rows)]
+#         count=0
+#         for i in range(rows):
+#             for j in range(cols):
+#                 if grid[i][j]=='1' and visited[i][j]==0:
+#                     count+=1
+#                     self.bfs(i,j,grid,visited)
+#         return count
+
+class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = [[0]*cols for _ in range(rows)]
+            
+        def dfs(i,j,visited):
+            visited[i][j]=1
+            for dx, dy in ((-1,0),(1,0),(0,-1),(0,1)):
+                nr = i+dx
+                nc = j+dy
+
+                if nr>=0 and nr<row and nc>=0 and nc<col and visited[nr][nc]!=1 and grid[nr][nc]!='0':
+                    dfs(nr,nc,visited)
+
+        row = len(grid)
+        col = len(grid[0])
+        visited = [[0]*col for _ in range(row)]
         count=0
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j]=='1' and visited[i][j]==0:
                     count+=1
-                    self.bfs(i,j,grid,visited)
+                    dfs(i,j,visited)
+
         return count
