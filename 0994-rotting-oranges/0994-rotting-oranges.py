1acc83756da8036be692a44df34b6e4137b759c5
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-09-20 22:40:14 +0530

    Time: 2 ms (81.09%), Space: 19.5 MB (13.74%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,32 @@
-from collections import deque
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        row=len(grid)
-        col=len(grid[0])
-        new_grid=deepcopy(grid)
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        q = deque()
         fresh=0
-        queue=deque()
-        for r in range(row):
-            for c in range(col):
-                if new_grid[r][c]==2:
-                    queue.append((r,c))
-                elif new_grid[r][c]==1:
+        time=0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]==2:
+                    q.append((i,j))
+                if grid[i][j]==1:
                     fresh+=1
-        minutes=0
-        while queue and fresh>0:
-            minutes+=1
-            rotten=len(queue)
-            for _ in range(rotten):
-                r,c=queue.popleft()
-                for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-                    new_r , new_c = dx+r, dy+c
-                    if new_r<0 or new_r==row or new_c<0 or new_c==col:
-                        continue
-                    if new_grid[new_r][new_c]==0 or new_grid[new_r][new_c]==2:
-                        continue
-                    fresh-=1
-                    queue.append((new_r,new_c))
-                    new_grid[new_r][new_c]=2
+        
+        while q and fresh>0:
+            time+=1
+            rotten = len(q)
+            while(rotten):
+                i,j = q.popleft()
+                for dx, dy in ((-1,0),(1,0),(0,-1),(0,1)):
+                    nr = dx+i
+                    nc = dy+j
+
+                    if nr>=0 and nr<row and nc>=0 and nc<col and grid[nr][nc]==1:
+                        q.append((nr,nc))
+                        fresh-=1
+                        grid[nr][nc]=-1
+                rotten-=1
+        
         if fresh>0:
             return -1
-        else:
-            return minutes
-            
+        return time
