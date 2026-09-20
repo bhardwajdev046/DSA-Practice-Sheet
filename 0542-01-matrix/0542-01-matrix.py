3677abe65433220924532157027b44a6a78b4311
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-09-21 01:48:22 +0530

    Time: 95 ms (88.8%), Space: 22.1 MB (68.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,26 +1,26 @@
-from collections import deque
 class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        row=len(mat)
-        col=len(mat[0])
-        visited=[[0]*(col) for _ in range(row)]
-        distance=[[0]*(col) for _ in range(row)]
-        queue=deque()
-        for r in range(row):
-            for c in range(col):
-                if mat[r][c]==0:
-                    queue.append([r,c,0])
-                    visited[r][c]=1
-        while queue:
-            i,j,d=queue.popleft()
-            distance[i][j]=d
-            for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-                new_r,new_c=(i+dx),(j+dy)
-                if new_r<0 or new_c<0 or new_r>=row or new_c>=col:
-                    continue
-                if visited[new_r][new_c]!=1:
-                    queue.append([new_r,new_c,d+1])
-                    visited[new_r][new_c]=1
-        return distance
+    def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
+        row = len(mat)
+        col = len(mat[0])
+        res=[[-1]*col for _ in range(row)]
+        q=deque()
 
+
+            
+
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j]==0:
+                    res[i][j]=0
+                    q.append((i,j))
+        while q:
+            r,c = q.popleft()
+            for dx,dy in ((-1,0),(1,0),(0,-1),(0,1)):
+                nr=r+dx
+                nc=c+dy
+                if nr>=0 and nr<row and nc>=0 and nc<col and res[nr][nc]==-1:
+                    res[nr][nc]=res[r][c]+1
+                    q.append((nr,nc))
+
+        return res
         
