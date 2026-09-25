0b8f9be5abaf85de8bbe6746a3f220f8b8e45bdb
Dev Bhardwaj <bhardwajdev046@gmail.com>
2026-09-25 21:15:44 +0530

    Time: 142 ms (68.76%), Space: 19.4 MB (99.7%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,27 +1,49 @@
 import heapq
 class Solution:
     def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
-        i,j = len(grid),len(grid[0])
-        if grid[0][0]==1 or grid[i-1][j-1]==1:
+        # i,j = len(grid),len(grid[0])
+        # if grid[0][0]==1 or grid[i-1][j-1]==1:
+        #     return -1
+
+
+        # dis = [[float('inf')]*j for _ in range(i)]
+
+        # heap=[(0,0,0)]
+        # dis[0][0]=0
+        # while heap:
+        #     d,r,c=heapq.heappop(heap)
+            
+            
+        #     if d>dis[r][c]:
+        #         continue
+        #     for dx,dy in ((-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1)):
+        #         nr = r+dx
+        #         nc = c+dy
+        #         if nr>=0 and nr<i and nc>=0 and nc<j and grid[nr][nc]==0:
+        #             if d+1 < dis[nr][nc]:
+        #                 dis[nr][nc]=d+1
+        #                 heapq.heappush(heap,(d+1,nr,nc))
+
+        # return -1 if dis[i-1][j-1]==float('inf') else dis[i-1][j-1]+1
+
+        row=len(grid)
+        col=len(grid[0])
+        if grid[0][0]==1:
             return -1
-
-
-        dis = [[float('inf')]*j for _ in range(i)]
-
-        heap=[(0,0,0)]
+        dis = [[float('inf')]*col for _ in range(row)]
+        q=deque()
+        q.append((0,0,0))
         dis[0][0]=0
-        while heap:
-            d,r,c=heapq.heappop(heap)
-            
-            
-            if d>dis[r][c]:
-                continue
-            for dx,dy in ((-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1)):
-                nr = r+dx
-                nc = c+dy
-                if nr>=0 and nr<i and nc>=0 and nc<j and grid[nr][nc]==0:
-                    if d+1 < dis[nr][nc]:
-                        dis[nr][nc]=d+1
-                        heapq.heappush(heap,(d+1,nr,nc))
 
-        return -1 if dis[i-1][j-1]==float('inf') else dis[i-1][j-1]+1
+        while q:
+            d,r,c = q.popleft()
+            
+            for dx,dy in ((-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1)):
+                nr=r+dx
+                nc=c+dy
+
+                if nr>=0 and nr<row and nc>=0 and nc<col and grid[nr][nc]==0:
+                    if d+1 < dis[nr][nc]:
+                        q.append((d+1,nr,nc))
+                        dis[nr][nc]=d+1
+        return -1 if dis[row-1][col-1]==float('inf') else dis[row-1][col-1]+1
